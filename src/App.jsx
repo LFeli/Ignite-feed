a645db87cac0da6c1f5b6cb1846a949d0a3c0451
Lucas Felipe <lucasfelipemk@gmail.com>
2023-12-14 23:11:37 -0300

    feat: Import sidebar component and some style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './global.css';
+import styles from './App.module.css';
 
 import { Header } from "./components/header/Header"
+import { Sidebar } from './components/sidebar/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        
+        <main>
+          Main
+        </main>
+      </div>
     </div>
   )
 }
