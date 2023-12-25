ac28d3d8167571686ffb1b5aa9aaa6d445e53390
Lucas Felipe <lucasfelipemk@gmail.com>
2023-12-25 20:45:10 -0300

    fix: Added missing key prop when use map

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -53,9 +53,9 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map( line => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           } else if (line.type === 'link'){
-            <p><a href="#">{line.content}</a></p>
+            <p key={line.content}><a href="#">{line.content}</a></p>
           }   
         })}
       </div>
@@ -77,7 +77,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map( comment => {
-          return <Comment content={comment} />
+          return <Comment key={comment} content={comment} />
         })}
       </div>
     </article>
